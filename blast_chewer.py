0f6d23a142014764a0f3c146a239e6915ce1d853
Marjanka <marie.pazoutova@gmail.com>
2023-07-12 15:42:18 +0200

    parsing data started

--- blast_chewer.py
+++ blast_chewer.py
@@ -15,16 +15,33 @@ def parse_arguments():
     return parser.parse_args()
 
 def read_file(input_path):
+    table = []
     with open(input_path, "r", encoding="utf-8") as infile:
         # after opening the file, we want to read it as a csv table, thus we introduce a new "input file" variable for that
         csv_file = csv.reader(infile, delimiter= "\t")
-        i = 1
         for row in csv_file:
-            i += 1
-            if i > 3:
-                break 
-            print(row)
+            table.append(row)
+        return table
 
+# we only need some columns from the file, namely 1, 2, 3, 12, 13 (qseqid, sseqid, stitle, evalue, bitscore)
+# plus we want to a) turn num values into float and b) extract only one hieararchical level from taxonomy (see parameter level=4)
+def parse_data(table, level=4):
+    result = []
+    for row in table:
+        qseqid = row[0]
+        sseqid = row[1]
+        taxonomy = row[2]
+        taxonomy = taxonomy[taxonomy.index(" ")+1:].split("__")
+        try:
+            taxonomy = taxonomy[level-1]
+        except:
+            taxonomy = taxonomy[0]
+        if taxonomy != "Diplomonadida":
+            print(taxonomy, end=" ") # TODO: delete this line
+        evalue = float(row[-2])
+        bitscore = float(row[-1])
+        result.append([qseqid, sseqid, taxonomy, evalue, bitscore])
+    return result
 
 if __name__ == "__main__":
     args = parse_arguments()
@@ -39,5 +56,9 @@ if __name__ == "__main__":
         # TODO: change exception to more specific 
         raise Exception (f"Input file {input_path} not valid.")
     
-    working_table = read_file(input_path)
+    data = read_file(input_path)
+
+    data = parse_data(data)
+
+    # print(data)
 
